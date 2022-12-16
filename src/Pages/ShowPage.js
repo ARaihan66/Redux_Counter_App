512a09327267beea5b0ebae97de62e676e00654e
Abu Raihan <aburaihancse066@gmail.com>
2022-12-17 02:29:58 +0600

    Add Constant

--- src/Pages/ShowPage.js
+++ src/Pages/ShowPage.js
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 
 const ShowPage = () => {
     const [count, setCount] = useState();
+
+    const handleClick = () => {
+
+    }
     return (
         <div>
+            <button onClick={handleClick}>INCREMENT</button>
             <h2>COUNTER NUMBER: {count}</h2>
+            <button onClick={handleClick}>DECREMENT</button>
         </div>
     )
 }
